279cccaa80b3e066cae5bb97ba547e9e2e927b0f
Kovalenko Vasyl <kovalenkova26@gmail.com>
2021-12-25 00:46:59 +0200

    functionality of uploading files to the app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./navbar/Navbar";
 import './app.scss';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Registration from '../components/authorization/Registration';
 import Login from '../components/authorization/Login';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { auth } from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
   const isAuth = useSelector(state => state.user.isAuth);
@@ -21,10 +22,16 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="wrap">
-        {!isAuth &&
+        {!isAuth ?
           <Switch>
             <Route path="/registration" component={Registration}/>
             <Route path="/login" component={Login}/>
+            <Redirect to="/login" />
+          </Switch>
+          :
+          <Switch>
+            <Route exact path="/" component={Disk}/>
+            <Redirect to="/" />
           </Switch>
         }
         
---
+++ client/src/components/disk/Disk.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFiles } from '../../actions/file';
+
+const Disk = () => {
+  const dispatch = useDispatch();
+  const currentDir = useSelector(state => state.files.currentDir);
+
+  useEffect(() => {
+    dispatch(getFiles(currentDir));
+  }, [currentDir]);
+  return (
+    <div>
+      disk
+    </div>
+  )
+}
+
+export default Disk;
